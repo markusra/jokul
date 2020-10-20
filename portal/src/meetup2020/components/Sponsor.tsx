f26da415ebe0b757772e6b14178e68f5d2bd966f
Eduardo <eduardo.rodriguez@fremtind.no>
2020-10-20 14:38:43 +0200

    docs(meetup sponsor improved): meetup sponsor improved
    affects: @fremtind/portal

--- portal/src/meetup2020/components/Sponsor.tsx
+++ portal/src/meetup2020/components/Sponsor.tsx
@@ -5,7 +5,15 @@ const Sponsor = () => {
     return (
         <div className="row row--topmargin">
             <article className="meetup-sponsor">
-                <p>TODO: SPONSOR</p>
+                <h1>Sponsor</h1>
+                <a
+                    className="meetup-sponsor meetup-sponsor__image"
+                    href="https://www.fremtind.no"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img src="assets/img/Fremtind_neg.svg" alt="Fremtind logo" />
+                </a>
             </article>
         </div>
     );
