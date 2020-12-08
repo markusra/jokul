33b32ab5c8a306c614c53120dbefc6649d73cc60
Markus Rauhut <markus.rauhut@fremtind.no>
2020-12-08 14:21:54 +0100

    feat(validators): add 'digits' argument to isValidRegistreringsnummer validator
    affects: @fremtind/jkl-validators-util
    
    Allows for validation of registreringsnummer with both four and five digits.
    
    BREAKING CHANGE:
    Need to add 'digits' argument to validator calls

--- packages/validators-util/src/isValidRegistreringsnummer/isValidRegistreringsnummer.test.ts
+++ packages/validators-util/src/isValidRegistreringsnummer/isValidRegistreringsnummer.test.ts
@@ -1,11 +1,48 @@
 import { isValidRegistreringsnummer } from "./isValidRegistreringsnummer";
 
+const isValidBilRegistreringsnummer = (regnummer: string) => isValidRegistreringsnummer(regnummer, 5);
+const isValidMcRegistreringsnummer = (regnummer: string) => isValidRegistreringsnummer(regnummer, 4);
+
 describe("isValidRegistreringsnummer", () => {
-    it("should return true for two letters and 5-digit numbers", () => {
-        expect(isValidRegistreringsnummer("AB12345")).toBe(true);
+    describe("bil", () => {
+        it("should return true for two letters and 5-digit numbers", () => {
+            expect(isValidBilRegistreringsnummer("AB12345")).toBe(true);
+        });
+
+        it("should return false if the string is empty", () => {
+            expect(isValidBilRegistreringsnummer("")).toBe(false);
+        });
+
+        it("should return false if only one letter", () => {
+            expect(isValidBilRegistreringsnummer("A12345")).toBe(false);
+        });
+
+        it("should return false if the string contains more than two letters and 5-digit numbers", () => {
+            expect(isValidBilRegistreringsnummer("AA12345jibberish")).toBe(false);
+            expect(isValidBilRegistreringsnummer("AA12345 jibberish")).toBe(false);
+            expect(isValidBilRegistreringsnummer("jibberish AA 12345")).toBe(false);
+            expect(isValidBilRegistreringsnummer("AA 12345")).toBe(false);
+        });
     });
 
-    it("should return false if only one letter and more than 5-digit numbers", () => {
-        expect(isValidRegistreringsnummer("A1234567")).toBe(false);
+    describe("MC", () => {
+        it("should return true for two letters and 4-digit numbers", () => {
+            expect(isValidMcRegistreringsnummer("AB1234")).toBe(true);
+        });
+
+        it("should return false if the string is empty", () => {
+            expect(isValidMcRegistreringsnummer("")).toBe(false);
+        });
+
+        it("should return false if only one letter", () => {
+            expect(isValidMcRegistreringsnummer("A1234")).toBe(false);
+        });
+
+        it("should return false if the string contains more than two letters and 4-digit numbers", () => {
+            expect(isValidMcRegistreringsnummer("AA12345jibberish")).toBe(false);
+            expect(isValidMcRegistreringsnummer("AA12345 jibberish")).toBe(false);
+            expect(isValidMcRegistreringsnummer("jibberish AA 12345")).toBe(false);
+            expect(isValidMcRegistreringsnummer("AA 12345")).toBe(false);
+        });
     });
 });
--- packages/validators-util/src/isValidRegistreringsnummer/isValidRegistreringsnummer.ts
+++ packages/validators-util/src/isValidRegistreringsnummer/isValidRegistreringsnummer.ts
@@ -1,3 +1,6 @@
-export const REGNR_REGEX = /^[a-zA-Z]{2}\d{5}/;
-
-export const isValidRegistreringsnummer = (value: string) => REGNR_REGEX.test(value);
+export const isValidRegistreringsnummer = (regnummer: string, digits: 4 | 5) => {
+    if (digits !== 4 && digits !== 5) {
+        throw new TypeError("Argument 'digits' is not valid. The number of digits must be either 4 or 5.");
+    }
+    return new RegExp(`^[a-z]{2}\\d{${digits}}$`, "i").test(regnummer);
+};
